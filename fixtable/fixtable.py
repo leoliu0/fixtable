c0297e16c1ce1691ff569ab970d9d7711ce19e4b
Leo Liu <auleo.liu@gmail.com>
2020-12-29 16:53:48 +1100

    version 0.3

--- fixtable/fixtable.py
+++ fixtable/fixtable.py
@@ -2,55 +2,70 @@
 import sys
 import re
 
-if __name__=='__main__':
-    changes = [('Observations','No. of Obs'),('R-squared','$R^2$')]
+changes = [('Observations','Obs.'),
+           ('Adjusted R-squared','Adj. $R^2$'),
+           ('R-squared','$R^2$'),
+           ]
 
-    try:
-        with open(sys.argv[2],'r') as v:
-            var = [x.strip().split(' ',1) for x in v.readlines() if x.strip()]
-    except:
-        var = None
+try:
+    with open(sys.argv[2],'r') as v:
+        var = [x.strip().split(' ',1) for x in v.readlines() if x.strip()]
+except:
+    var = None
 
-    try:
-        f = sys.argv[1]
-    except:
-        raise ValueError('you must specify a filename to parse')
-    with open(f,'r') as file:
-        c = file.readlines()
+f = sys.argv[1]
+with open(f,'r') as file:
+    c = file.readlines()
 
-    output = []
-    for row in c:
-        for x,y in changes:
+output = []
+for row in c:
+    for x,y in changes:
+        row = row.replace(x,y)
+    if var:
+        for x,y in var:
             row = row.replace(x,y)
-        if var:
-            for x,y in var:
-                row = row.replace(x,y)
-        if 'multicolumn' in row:
-            continue
-        if '1o' in row or '0b' in row:
-            continue
-        if 'tabular' in row:
-            continue
-        if '{document}' in row:
-            continue
-        if 'documentclass' in row:
-            continue
-        if 'setlength' in row:
-            continue
-        if '(.)' in row:
-            continue
-        if '& . &' in row:
-            continue
-        if '& . \\' in row:
-            continue
-        if '& (.) &' in row:
-            continue
-        row = re.sub(r'(\d+)(\*+)',r'\1&\2',row)
-        row = re.sub(r'(?<![\*\\])\s*&(?!\s*\*)',r'&&',row)
-        row = re.sub(r'(?<!\*)\s*\\\\',r'&\\\\',row)
-        row = re.sub(r'\*\s+&','*',row)
-        row = re.sub(r'&&',r'&',row,1)
-        output.append(row)
+    if 'multicolumn' in row:
+        continue
+    if '1o' in row or '0b' in row:
+        continue
+    if 'tabular' in row:
+        continue
+    if '{document}' in row:
+        continue
+    if 'documentclass' in row:
+        continue
+    if 'setlength' in row:
+        continue
+    if '(.)' in row:
+        continue
+    if '& . &' in row:
+        continue
+    if '& . \\' in row:
+        continue
+    if '& (.) &' in row:
+        continue
+    row = re.sub(r'(\d+)(\*+)',r'\1&\2',row)
+    row = re.sub(r'(?<![\*\\])\s*&(?!\s*\*)',r'&&',row)
+    row = re.sub(r'(?<!\*)\s*\\\\',r'&\\\\',row)
+    row = re.sub(r'\*\s+&','*',row)
+    row = re.sub(r'&&',r'&',row,1)
+    output.append(row)
 
-    for row in output:
-        print(row.strip())
+buffers = []
+for row in output:
+    r = row.strip()
+    if re.search(r'^Obs',r):
+        buffers.append(row)
+        continue
+    if re.search(r'^\$R^2',r):
+        buffers.append(row)
+        continue
+    if re.search(r'^A',r):
+        buffers.append(r)
+        continue
+    print(r)
+
+print('\\\\')
+for row in buffers:
+    print(row.strip().replace('\hline',''))
+print('\hline')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fixtable',         # How you named your package folder (MyLib)
   packages = ['fixtable'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Fix outreg2 table from stata',   # Give a short description about your library
   author = 'Leo Liu',                   # Type in your name
@@ -10,10 +10,10 @@ setup(
   url = 'https://github.com/leoliu0/fixtable',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   #  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  #  install_requires=[
           #  'validators',
           #  'beautifulsoup4',
-      ],
+      #  ],
   scripts = ['fixtable/fixtable.py'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
