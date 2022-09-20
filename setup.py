4a2bf14998a1e51847fa259c24b4f07fe0c4176c
Leo Liu <auleo.liu@gmail.com>
2022-09-20 16:09:34 +1000

    update

--- setup.py
+++ setup.py
@@ -1,35 +1,30 @@
 from distutils.core import setup
 
 setup(
-    name='fixtable',  # How you named your package folder (MyLib)
-    packages=['fixtable'],  # Chose the same as "name"
-    version=
-    '0.4',  # Start with a small number and increase it with every change you make
-    license=
-    'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description=
-    'Fix outreg2 table from stata',  # Give a short description about your library
-    author='Leo Liu',  # Type in your name
-    author_email='auleo.liu@gmail.com',  # Type in your E-Mail
-    url=
-    'https://github.com/leoliu0/fixtable',  # Provide either the link to your github or to your website
-    download_url=
-    'https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+    name="fixtable",  # How you named your package folder (MyLib)
+    packages=["fixtable"],  # Chose the same as "name"
+    version="0.4",  # Start with a small number and increase it with every change you make
+    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description="Fix outreg2 table from stata",  # Give a short description about your library
+    author="Leo Liu",  # Type in your name
+    author_email="auleo.liu@gmail.com",  # Type in your E-Mail
+    url="https://github.com/leoliu0/fixtable",  # Provide either the link to your github or to your website
+    download_url="https://github.com/user/reponame/archive/v_01.tar.gz",  # I explain this later on
     #  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
     #  install_requires=[
     #  'validators',
     #  'beautifulsoup4',
     #  ],
-    scripts=['fixtable/fixtable'],
+    scripts=["fixtable/fixtable"],
     classifiers=[
-        'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
-        'Programming Language :: Python :: 3',  #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
