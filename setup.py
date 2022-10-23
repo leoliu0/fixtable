e3f7ef66ae801599b6a0d9a8e5923525f748cf51
Leo Liu <auleo.liu@gmail.com>
2022-10-23 14:59:42 +1100

    windows

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     #  'validators',
     #  'beautifulsoup4',
     #  ],
-    scripts=["fixtable/fixtable"],
+    scripts=["fixtable/fixtable.py"],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Intended Audience :: Developers",  # Define that your audience are developers
